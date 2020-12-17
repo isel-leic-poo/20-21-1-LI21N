e0adde8338e6e2224f69f3ec8069425dc3a110fb
Pedro Pereira <palex@cc.isel.ipl.pt>
2020-12-17 23:50:58 +0000

    Aula 17 Dezembro
    Rotação de ecran.

--- Draw/app/src/main/java/pt/isel/poo/draw/DrawActivity.java
+++ Draw/app/src/main/java/pt/isel/poo/draw/DrawActivity.java
@@ -1,5 +1,6 @@
 package pt.isel.poo.draw;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -11,21 +12,20 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 import pt.isel.poo.draw.model.Picture;
 
 public class DrawActivity extends AppCompatActivity {
-
     public static final String FILE_NAME = "picture.txt";
+    public static final String TAG = "DRAW";
+    public static final String KEY_MODEL = "Picture";
+
     Button clear, undo, save, load;
     DesignView view;
     Picture model = new Picture();
@@ -33,6 +33,7 @@ public class DrawActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(TAG,"onCreate("+savedInstanceState+")");
         view = new DesignView(this);
         setContentView(initView());
         clear.setOnClickListener(v -> {model.clear(); view.setModel(model);});
@@ -41,12 +42,24 @@ public class DrawActivity extends AppCompatActivity {
         load.setOnClickListener( v -> load() );
         view.setModel(model);
     }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.v(TAG,"onRestoreInstanceState");
+        try (Scanner s = new Scanner(new ByteArrayInputStream(savedInstanceState.getByteArray(KEY_MODEL)))) {
+            model.load(s);
+        } catch (IOException ex) {
+            Log.e(TAG,"Error in restore State.",ex);
+        }
+    }
+
     private void load() {
         try (Scanner s = new Scanner(openFileInput(FILE_NAME))) {
             model.load(s);
             view.invalidate();
         }catch (IOException ex) {
-            Log.e("DRAW","Error reading file",ex);
+            Log.e(TAG,"Error reading file",ex);
             Toast.makeText(this,"Error reading",Toast.LENGTH_LONG).show();
         }
     }
@@ -57,7 +70,7 @@ public class DrawActivity extends AppCompatActivity {
             ))) {
             model.save(bw);
         } catch (IOException ex) {
-            Log.e("DRAW","Error creating file",ex);
+            Log.e(TAG,"Error creating file",ex);
         }
     }
 
@@ -82,5 +95,26 @@ public class DrawActivity extends AppCompatActivity {
         root.addView(view);
         bar.setBackgroundColor(Color.LTGRAY);
         return root;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.v(TAG, "onSaveInstanceState");
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(baos));
+            model.save(bw);
+            bw.close();
+            outState.putByteArray(KEY_MODEL,baos.toByteArray());
+        } catch (IOException ex) {
+            Log.e(TAG,"Error write in Bundle",ex);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG,"onDestroy");
     }
 }
